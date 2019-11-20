0837717816fa4c1a93a2cddf427dd64e8b0f99ae
William Barkoff <william@barkoffusa.com>
2019-11-19 21:38:44 -0500

    add hash routing (client-side navigation)

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,32 @@ interface AppState {
 export default class App extends React.Component<{}, AppState> {
 	constructor(props: {}) {
 		super(props)
+		let currentRun = window.location.hash.substr(1);
 		this.state = {
-			currentRun: null
+			currentRun: currentRun
 		}
+
+		window.onpopstate = this.updateRun.bind(this)
 	}
 
 	selectRun(filename: string) {
 		this.setState({
 			currentRun: filename
 		})
+		history.pushState(null, filename, "#/" + filename)
+	}
+
+	updateRun() {
+		let currentRun = window.location.hash.substr(1);
+		if (currentRun) {
+			this.setState({
+				currentRun: currentRun
+			})
+		} else {
+			this.setState({
+				currentRun: null
+			})
+		}
 	}
 
 	render() {
